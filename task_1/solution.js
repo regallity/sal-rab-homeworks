4878899a875cb4ade20aceafb6e4e4f2d32cd8fc
Elena Lazarenko <regallity@list.ru>
2021-08-17 21:58:36 +0300

    message

--- task_1/solution.js
+++ task_1/solution.js
@@ -5,6 +5,8 @@ function addInCartMessage(title, price) {
     // Задание №1.1. Оповещение о добавлении в корзину
 
     // создайте переменную message
+    let message;
+    
     // присвойте переменной message значение переменной productName
     // добавьте в конец переменной message пробел
     // добавьте в конец переменной message текст 'за'
