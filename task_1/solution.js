e41e40abb916fd72ff20a83fe15b8e39b9b1b404
Elena Lazarenko <regallity@list.ru>
2021-08-17 22:11:34 +0300

    new

--- task_1/solution.js
+++ task_1/solution.js
@@ -34,6 +34,7 @@ function addInCartCountChange(value) {
     let oldValue = value;
 
     // создайте переменную newValue
+    let newValue;
     // присвойте ей значение oldValue
     // увеличьте значение newValue на 1
 
