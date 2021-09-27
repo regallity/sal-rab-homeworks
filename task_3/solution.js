2619aa8d6fdd8137ad58230839ee4afa04ece48d
Elena Lazarenko <regallity@list.ru>
2021-09-27 22:22:01 +0300

    count

--- task_3/solution.js
+++ task_3/solution.js
@@ -23,6 +23,7 @@ function renderCartItem(item) {
     // 7. Присвойте переменной itemCountText результат склеивания itemCountText и sum
     // 8. Присвойте переменной itemCountText результат склеивания itemCountText и ` ₽`
 let itemCountText ="";
+itemCountText+=product.count;
 
     // Конец решения задания №3.1.
 
