80176090103f2870208ea20f16edb92eff090f0f
Elena Lazarenko <regallity@list.ru>
2021-09-27 22:00:45 +0300

    sozdanieItem

--- task_3/solution.js
+++ task_3/solution.js
@@ -22,6 +22,7 @@ function renderCartItem(item) {
     // 6. Создайте переменную sum и присвойте ей значение произведения количества и цены
     // 7. Присвойте переменной itemCountText результат склеивания itemCountText и sum
     // 8. Присвойте переменной itemCountText результат склеивания itemCountText и ` ₽`
+let itemCountText ="";
 
     // Конец решения задания №3.1.
 
