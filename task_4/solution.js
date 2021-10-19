84b6a027cccf39a7f7835b323ae1027069e02f21
Elena Lazarenko <regallity@list.ru>
2021-10-19 22:53:11 +0300

    json

--- task_4/solution.js
+++ task_4/solution.js
@@ -3,6 +3,7 @@
 function parseProducts(json) {
     // Аргументом функции является JSON
 let data;
+data=JSON.parse(json);
     // Преобразуйте строку json, переданную как аргумент функции,
     // в объект с помощью функции JSON.parse(json)
     // и запишите в переменную data
